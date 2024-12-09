fe36a5dfea4d8bebb3272dbd7005b2bc46896e3f
Adam Horo <loginadas@gmail.com>
2024-12-09 21:19:47 +0100

    revert

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PartyTavern.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
