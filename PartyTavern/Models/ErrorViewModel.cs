2a227115f3bd7e4ebc6f04599174734cf237cc00
Adam Horo <loginadas@gmail.com>
2024-12-09 21:24:20 +0100

    wstępne pliki projektowe

---
+++ PartyTavern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PartyTavern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
